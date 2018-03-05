4be5377ff89bd695eedce737aa86dfc8beabd3a7
mad <MadAraujo>
2018-03-05 14:40:55 -0600

    Cargando Parcial1

---
+++ Ene-Jun-2018/Mad Araujo/Parcial 1/primerParcial.go
@@ -0,0 +1,33 @@
+package main
+
+import("bufio"
+    "fmt"
+    "os"
+    "strings")
+
+func main(){
+reader := bufio.NewReader(os.Stdin) // para capturar string
+fmt.Print("Escriba una Palabra  ") // solo un input
+        palabra, _ := reader.ReadString('\n') // capturar string        
+        palabra = strings.Replace(palabra, "\n", "", -1)
+
+fmt.Print("Escriba una Vocal  ") // solo un input
+        vocal, _ := reader.ReadString('\n') // capturar string
+        vocal = strings.Replace(vocal, "\n", "", -1)
+
+     resultadoe := strings.Replace(palabra,"e", vocal, -1) // cambio vocal por vocal y almaceno
+     resultadoi := strings.Replace(resultadoe, "i", vocal, -1)// cambio vocal por vocal y almaceno
+     resultadoo := strings.Replace(resultadoi, "o", vocal, -1)// cambio vocal por vocal y almaceno
+     resultadou := strings.Replace(resultadoo, "u", vocal, -1)// cambio vocal por vocal y almaceno
+ 			
+        userFile := "pragramacaan.txt"
+		fout, err := os.Create(userFile)		
+		if err != nil {
+			fmt.Println(userFile, err)
+			return
+		}
+		defer fout.Close()
+		fout.WriteString(resultadou)    
+
+}//cierro main
+
